f6cfe52742631f4d031e20893329ee4a6c7cb5b2
Mary Paskhaver <uzhinobed@gmail.com>
2020-12-30 12:24:06 -0500

    Add move methods to MovableTile class, extend to Player

--- This Harmony/MovableTile.swift
+++ This Harmony/MovableTile.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SpriteKit
+
+class MovableTile: SKSpriteNode {
+    
+    func moveRight(byNumTiles numTiles: Int) {
+        self.run(SKAction.moveBy(x: CGFloat(numTiles * 80), y: 0, duration: 0.2))
+    }
+    
+    func moveLeft(byNumTiles numTiles: Int) {
+        self.run(SKAction.moveBy(x: CGFloat(-numTiles * 80), y: 0, duration: 0.2))
+    }
+    
+    func moveUp(byNumTiles numTiles: Int) {
+        self.run(SKAction.moveBy(x: 0, y: CGFloat(numTiles * 80), duration: 0.2))
+    }
+    
+    func moveDown(byNumTiles numTiles: Int) {
+        self.run(SKAction.moveBy(x: 0, y: CGFloat(-numTiles * 80), duration: 0.2))
+    }
+}
