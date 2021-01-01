80bf11e47c622877d02cf211aa04ce881bc6222b
Mary Paskhaver <uzhinobed@gmail.com>
2020-12-31 20:05:04 -0500

    Create Floor class w/ player + crate properties

---
+++ This Harmony/Floor.swift
@@ -0,0 +1,14 @@
+//
+//  Floor.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 12/31/20.
+//
+
+import Foundation
+import SpriteKit
+
+class Floor: SKSpriteNode {
+    var player: SKSpriteNode?
+    var crate: SKSpriteNode?
+}
---
+++ This Harmony/Grid.swift
@@ -0,0 +1,8 @@
+//
+//  Grid.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 12/31/20.
+//
+
+import Foundation
