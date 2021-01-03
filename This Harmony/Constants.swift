9b36c7b6f004fafebc36521c53d3c6eb3116d6eb
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-02 22:03:27 -0500

    Add TileNames enum property

--- This Harmony/Constants.swift
+++ This Harmony/Constants.swift
@@ -10,4 +10,10 @@ import Foundation
 class Constants {
     static let tileSize: Int = 80
     static let movementAnimationDuration: TimeInterval = 0.2
+    
+    enum TileNames: String {
+        case floor = "floor"
+        case crate = "crate"
+        case wall = "wall"
+    }
 }
