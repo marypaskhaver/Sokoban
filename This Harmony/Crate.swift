c72f67867ec54577e0e19024a60781b9fef0a732
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-04 15:58:42 -0500

    Add isOnStorageArea bool + update image based on it

--- This Harmony/Crate.swift
+++ This Harmony/Crate.swift
@@ -6,7 +6,18 @@
 //
 
 import Foundation
+import SpriteKit
 
 class Crate: MovableTile {
-
+    var isOnStorageArea: Bool = false
+    
+    func updateImage() {
+        if isOnStorageArea {
+//            self.texture = SKTexture(imageNamed: "crateOnStorage")
+            print("changing image to crateOnStorage")
+        } else {
+            self.texture = SKTexture(imageNamed: "crate")
+            print("changing image to default crate")
+        }
+    }
 }
