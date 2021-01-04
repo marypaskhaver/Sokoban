f783fdb1557157c312048bb50826bc6c7a6f6fb6
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-04 15:55:34 -0500

    Create Storage.swift + override setCrate method from Floor.swift

---
+++ This Harmony/Storage.swift
@@ -0,0 +1,26 @@
+//
+//  Storage.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 1/4/21.
+//
+
+import Foundation
+import SpriteKit
+
+class Storage: Floor {    
+    init() {
+        super.init(withTexture: SKTexture(imageNamed: "storage"))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func setCrate(to crate: Crate) {
+        crate.isOnStorageArea = true
+        crate.updateImage()
+        self.crate = crate
+    }
+    
+}
