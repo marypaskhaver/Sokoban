e01db3f3efbd6b4c82288babf8847b7e8b911a20
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-06 22:07:54 -0500

    Create MSButtonNode class, use for play button on MainMenu

--- This Harmony/MSButtonNode.swift
+++ This Harmony/MSButtonNode.swift
@@ -1,8 +1,65 @@
 //
 //  MSButtonNode.swift
-//  This Harmony
+//  Make School
 //
-//  Created by Mary Paskhaver on 1/6/21.
+//  Copyright (c) 2017 Make School. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+enum MSButtonNodeState {
+    case MSButtonNodeStateActive, MSButtonNodeStateSelected, MSButtonNodeStateHidden
+}
+
+class MSButtonNode: SKSpriteNode {
+    
+    // Setup a dummy action closure
+    var selectedHandler: () -> Void = { print("No button action set") }
+    
+    // Button state management
+    var state: MSButtonNodeState = .MSButtonNodeStateActive {
+        didSet {
+            switch state {
+            case .MSButtonNodeStateActive:
+                // Enable touch
+                self.isUserInteractionEnabled = true
+                
+                // Visible
+                self.alpha = 1
+                break
+            case .MSButtonNodeStateSelected:
+                // Semi transparent
+                self.alpha = 0.7
+                break
+            case .MSButtonNodeStateHidden:
+                // Disable touch
+                self.isUserInteractionEnabled = false
+                
+                // Hide
+                self.alpha = 0
+                break
+            }
+        }
+    }
+    
+    // Support for NSKeyedArchiver (loading objects from SK Scene Editor)
+    required init?(coder aDecoder: NSCoder) {
+        
+        // Call parent initializer e.g. SKSpriteNode
+        super.init(coder: aDecoder)
+        
+        // Enable touch on button node
+        self.isUserInteractionEnabled = true
+    }
+    
+    // MARK: - Touch handling
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        state = .MSButtonNodeStateSelected
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        selectedHandler()
+        state = .MSButtonNodeStateActive
+    }
+    
+}
