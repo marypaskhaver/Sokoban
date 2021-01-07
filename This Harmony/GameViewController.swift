5651084dacfb3003401fcfb7dc09d3a4b53f4964
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-06 22:08:56 -0500

    Load MainMenu scene first instead of GameScene

--- This Harmony/GameViewController.swift
+++ This Harmony/GameViewController.swift
@@ -15,8 +15,7 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = MainMenu(fileNamed: "MainMenu") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
