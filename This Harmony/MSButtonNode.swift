c1a389e6af75f4f0c9405afde972493837d9012a
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-06 22:04:13 -0500

    Add menu background + play button images

---
+++ This Harmony/MSButtonNode.swift
@@ -0,0 +1,8 @@
+//
+//  MSButtonNode.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 1/6/21.
+//
+
+import Foundation
---
+++ This Harmony/MainMenu.swift
@@ -0,0 +1,8 @@
+//
+//  MainMenu.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 1/6/21.
+//
+
+import Foundation
