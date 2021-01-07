d729f76d2198448221e832128ac6aa5cf6a69b2e
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-06 22:04:57 -0500

    Create MainMenu scene and file

--- This Harmony/MainMenu.swift
+++ This Harmony/MainMenu.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import SpriteKit
+
+class MainMenu: SKScene {
+
+    var buttonPlay: MSButtonNode!
+
+    override func didMove(to view: SKView) {
+        // Set UI connections
+        buttonPlay = self.childNode(withName: "buttonPlay") as! MSButtonNode
+        
+        buttonPlay.selectedHandler = {
+            self.loadGame()
+        }
+    }
+    
+    func loadGame() {
+        /* 1) Grab reference to our SpriteKit view */
+        guard let skView = self.view as SKView? else {
+            print("Could not get SKView")
+            return
+        }
+
+        guard let scene = GameScene(fileNamed:"GameScene") else {
+            print("Could not make GameScene, check the name is spelled correctly")
+            return
+        }
+
+        /* 3) Ensure correct aspect mode */
+        scene.scaleMode = .aspectFill
+
+        /* Show debug */
+        skView.showsPhysics = true
+        skView.showsDrawCount = true
+        skView.showsFPS = true
+
+        /* 4) Start game scene */
+        skView.presentScene(scene)
+    }
+}
