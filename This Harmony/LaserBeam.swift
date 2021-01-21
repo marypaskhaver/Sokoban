e726aa8a09c993f981a0ac690e629c5026a5f677
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-20 20:24:02 -0500

    Add laser pointer and beam images and classes

---
+++ This Harmony/LaserBeam.swift
@@ -0,0 +1,12 @@
+//
+//  LaserBeam.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 1/20/21.
+//
+
+import SpriteKit
+
+class LaserBeam: SKSpriteNode {
+    
+}
---
+++ This Harmony/LaserPointer.swift
@@ -0,0 +1,12 @@
+//
+//  LaserPointer.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 1/20/21.
+//
+
+import SpriteKit
+
+class LaserPointer: SKSpriteNode {
+    
+}
