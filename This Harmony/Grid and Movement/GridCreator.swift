824d0f6b6e3f3de69b1d65d876783a48c78844f1
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-21 12:38:29 -0500

    Move grid's laser funcs here, modify init

--- This Harmony/Grid and Movement/GridCreator.swift
+++ This Harmony/Grid and Movement/GridCreator.swift
@@ -10,9 +10,16 @@ import SpriteKit
 
 class GridCreator {
     var grid: [ [Tile] ] = [ [Tile] ]()
-    var childrenToAddToView: [Floor : CGPoint] = [Floor : CGPoint]()
+    var laserPointers: [LaserPointer] = [LaserPointer]()
+    var childrenToAddToView: [Tile : CGPoint] = [Tile : CGPoint]()
     
-    func getGridOfScenesChildren(_ children: [SKNode]) -> [ [Tile] ] {
+    init(withChildren children: [SKNode]) {
+        _ = getGridOfScenesChildren(children)
+        laserPointers = getLaserPointerNodesFromScenesChildren(children)
+        activateLaserBeams()
+    }
+    
+    private func getGridOfScenesChildren(_ children: [SKNode]) -> [ [Tile] ] {
         var arrayOfNodes: [Tile] = []
         
         for child in children {
@@ -41,7 +48,7 @@ class GridCreator {
         return grid
     }
     
-    func getLaserPointerNodesFromScenesChildren(_ children: [SKNode]) -> [LaserPointer] {
+    private func getLaserPointerNodesFromScenesChildren(_ children: [SKNode]) -> [LaserPointer] {
         var laserPointers: [LaserPointer] = []
 
         for child in children {
@@ -54,6 +61,73 @@ class GridCreator {
         }
         
         return laserPointers
+    }
+    
+    // Find where laser beams should go and add them to scene
+    func activateLaserBeams() {
+        // Find laser pointer nodes and their row/col in the grid
+        for row in 0..<grid.count {
+            for col in 0..<grid[row].count {
+                for lp in laserPointers {
+                    if grid[row][col].frame.midX == lp.frame.midX && grid[row][col].frame.midY == lp.frame.midY {
+                        // For each laser pointer, check its rotation to find out which direction to place the beams (0 deg = pointing up, then -90 deg turns clockwise)
+                        // Check how far a clear path extends in that direction on the grid-- that is, clear rows and cols (laser cannot go thru walls or crates)
+                        let clearTiles: [Floor] = getLongestClearPathOfFloor(from: Point(row: row, col: col), inDirection: lp.direction)
+                        
+                        // For each of the clear tiles in the grid before the pointer, place a laser beam node rotated to the proper amount there
+                        for tile in clearTiles {
+                            let laserBeam: LaserBeam = LaserBeam(inDirection: lp.direction, atPoint: tile.position)
+                            // For the Floors the laser beam crosses over, set its laserBeam property to that laserBeam node
+                            tile.laserBeam = laserBeam
+                            childrenToAddToView[laserBeam] = laserBeam.position
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    func getLongestClearPathOfFloor(from point: Point, inDirection dir: Direction) -> [Floor] {
+        var row: Int = point.row, col: Int = point.col
+        var tilesInFront: [Floor] = [Floor]()
+        
+        switch dir {
+        case .up:
+            row -= 1 // Don't count wall which laser is attached to
+            // Check if path free of crates and walls-- does this cover all scenarios?
+            while (grid[row][col] as? Floor)?.crate == nil && grid[row][col].name != Constants.TileNames.wall.rawValue {
+                tilesInFront.append(grid[row][col] as! Floor)
+                row -= 1
+            }
+
+            print("collision up at \(row), \(col)")
+        case .down:
+            row += 1
+            while (grid[row][col] as? Floor)?.crate == nil && grid[row][col].name != Constants.TileNames.wall.rawValue {
+                tilesInFront.append(grid[row][col] as! Floor)
+                row += 1
+            }
+            print("collision down at \(row), \(col)")
+        case .left:
+            col -= 1
+            while (grid[row][col] as? Floor)?.crate == nil && grid[row][col].name != Constants.TileNames.wall.rawValue {
+                tilesInFront.append(grid[row][col] as! Floor)
+                col -= 1
+            }
+            
+            print("collision left at \(row), \(col)")
+        case .right:
+            col += 1
+            while (grid[row][col] as? Floor)?.crate == nil && grid[row][col].name != Constants.TileNames.wall.rawValue {
+                tilesInFront.append(grid[row][col] as! Floor)
+                col += 1
+            }
+
+            print("collision right at \(row), \(col)")
+        }
+        
+        return tilesInFront
     }
     
     // The way this is built, the player will never start the level on a storage area
@@ -71,7 +145,7 @@ class GridCreator {
                     let position: CGPoint = CGPoint(x: col * Constants.tileSize + 139, y: 900 - (row * Constants.tileSize))
                     (grid[row][col] as! Floor).position = position
 
-                    childrenToAddToView[grid[row][col] as! Floor] = CGPoint(x: col * Constants.tileSize + 139, y: 900 - (row * Constants.tileSize))
+                    childrenToAddToView[grid[row][col] as! Floor] = position
                 } else if grid[row][col].name == Constants.TileNames.crate.rawValue {
                     let crate: Crate = grid[row][col] as! Crate
                     
