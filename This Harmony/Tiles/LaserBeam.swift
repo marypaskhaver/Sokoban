1e5b9448a2ce0d273c2666168141a5009e66b7a4
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-21 12:38:06 -0500

    Add init to create self w/ texture at certain pt, rotate in right dir

--- This Harmony/Tiles/LaserBeam.swift
+++ This Harmony/Tiles/LaserBeam.swift
@@ -8,5 +8,27 @@
 import SpriteKit
 
 class LaserBeam: Tile {
+    var directionAndDegreeDict: [Direction : Int] = [Direction : Int]()
+    
+    func rotate(toBePointing dir: Direction) {
+        directionAndDegreeDict[.up] = 0
+        directionAndDegreeDict[.down] = 180
+        directionAndDegreeDict[.left] = 90
+        directionAndDegreeDict[.right] = -90
+                
+        // Convert degrees to zRotation radians
+        self.zRotation = CGFloat(directionAndDegreeDict[dir]!) * (.pi / 180)
+    }
+    
+    init(inDirection dir: Direction, atPoint pt: CGPoint) {
+        super.init(texture: SKTexture(imageNamed: "laser_beam_pointing_up"), name: Constants.TileNames.laserBeam.rawValue)
+        self.position = pt
+        rotate(toBePointing: dir)
+        self.zPosition = 1
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
 }
