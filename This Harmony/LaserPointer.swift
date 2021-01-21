2f022b3078d6fca340f245afa249a54616f657f4
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-20 22:25:56 -0500

    Inherit from tile, set direction property w/ func

--- This Harmony/LaserPointer.swift
+++ This Harmony/LaserPointer.swift
@@ -7,6 +7,17 @@
 
 import SpriteKit
 
-class LaserPointer: SKSpriteNode {
+class LaserPointer: Tile {
+    var direction: Direction!
+    var degreeAndDirectionDict: [Int : Direction] = [Int : Direction]()
     
+    func setDirection() {
+        degreeAndDirectionDict[0] = .up
+        degreeAndDirectionDict[-180] = .down
+        degreeAndDirectionDict[90] = .left
+        degreeAndDirectionDict[-90] = .right
+        
+        self.direction = degreeAndDirectionDict[Int(self.zRotation)]
+    }
+   
 }
