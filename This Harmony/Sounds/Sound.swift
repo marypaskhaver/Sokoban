909a93e37ce51248752684a61c504c53712a8b10
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-22 11:30:32 -0500

    Create Sound enum

---
+++ This Harmony/Sounds/Sound.swift
@@ -0,0 +1,12 @@
+//
+//  Sound.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 1/22/21.
+//
+
+import Foundation
+
+enum Sound: String {
+    case playerCantMove = "player_cant_move.wav"
+}
