75f4a36b23405292f1b5365c469eb891d99bfb48
Mary Paskhaver <uzhinobed@gmail.com>
2021-01-24 16:23:09 -0500

    Add Foundation and SpriteKit imports

--- This Harmony/Extensions/SKNode+RoundedPosition.swift
+++ This Harmony/Extensions/SKNode+RoundedPosition.swift
@@ -5,6 +5,9 @@
 //  Created by Mary Paskhaver on 1/24/21.
 //
 
+import Foundation
+import SpriteKit
+
 extension SKNode {
     func getRoundedX() -> CGFloat {
         return self.position.x.rounded()
