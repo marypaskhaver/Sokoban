83a008e0d4428c37a2d678ad9a5d4a481c2df002
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-03-03 14:16:59 -0500

    Test if level 1 exists and can't go to level before 1

--- This HarmonyTests/This_HarmonyTests.swift
+++ This HarmonyTests/This_HarmonyTests.swift
@@ -6,22 +6,40 @@
 //
 
 import XCTest
+import SpriteKit
+
 @testable import This_Harmony
 
 class This_HarmonyTests: XCTestCase {
-
+    var gc: GameViewController!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        gc = storyboard.instantiateViewController(identifier: "GameViewController") as? GameViewController
+        gc.loadViewIfNeeded()
+        
+        let skView = gc.view as! SKView
+        (skView.scene as! MainMenu).loadGame()
+        gc.loadViewIfNeeded()
+
+    }
+    
+    func testLevel1GridExist() {
+        let scene: GameScene = (gc.view as! SKView).scene as! GameScene
+        XCTAssertNotNil(scene.grid)
+    }
+    
+    func testCantGoToPrevLevelFromLevel1() {
+        let scene: GameScene = (gc.view as! SKView).scene as! GameScene
+        XCTAssert(scene.buttonPrevious.state == .disabled)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
