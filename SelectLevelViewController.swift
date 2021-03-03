97a7f50f9cec73371830f2d8210c3fd0be87236c
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-03-03 16:01:16 -0500

    Conform to collectionview dataSource, delegate + load numbered LevelCells

--- SelectLevelViewController.swift
+++ SelectLevelViewController.swift
@@ -8,6 +8,29 @@
 import UIKit
 import Foundation
 
-class SelectLevelViewController: UIViewController {
+class SelectLevelViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        print("hello!")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell: LevelCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! LevelCell
+        let cellNumber: Int = indexPath.row + 1
+        cell.levelNumberLabel.text = String(cellNumber)
+        
+        return cell
+    }
+    
+    
+    
     
 }
