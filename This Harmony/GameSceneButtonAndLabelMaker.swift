7e413d8ad8b2fd757def3e9f306a6d7d1e41bcd7
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-03-03 19:37:05 -0500

    Add init, make addButtonsAndLabels non-static, refactor to other funcs

--- This Harmony/GameSceneButtonAndLabelMaker.swift
+++ This Harmony/GameSceneButtonAndLabelMaker.swift
@@ -9,36 +9,49 @@ import Foundation
 import SpriteKit
 
 class GameSceneButtonAndLabelMaker {
-    static func addButtonsAndLabels(toGameScene gc: GameScene) {
+    var gc: GameScene!
+    
+    init(with gc: GameScene) {
+        self.gc = gc
+    }
+    
+    func addButtonsAndLabels() {
         let buttonRestart: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "reset_button"), CGSize(width: 80, height: 80), atPosition: CGPoint(x: 384, y: 60))
         let buttonNext: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "next_button"), CGSize(width: 80, height: 80), atPosition: CGPoint(x: 200, y: 60))
         let buttonPrevious: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "prev_button"), CGSize(width: 80, height: 80), atPosition: CGPoint(x: 100, y: 60))
         let buttonMenu: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "menu_button"), CGSize(width: 60, height: 60), atPosition: CGPoint(x: 80, y: 970))
-
-        buttonRestart.selectedHandler = {
-            gc.gvc.loadLevel(number: GameScene.level)
-        }
-        
-        buttonNext.selectedHandler = {
-            gc.goToNextLevel()
-        }
-        
-        buttonPrevious.selectedHandler = {
-            gc.goToPreviousLevel()
-        }
-        
-        buttonMenu.selectedHandler = {
-            gc.showPauseAndSettingsMenu()
-        }
-        
-        gc.levelLabel = TextLabel("Level \(GameScene.level)", at: CGPoint(x: 384, y: 970))
-        gc.stepsLabel = TextLabel("Steps: \(gc.grid.steps)", at: CGPoint(x: 384, y: 150))
         
         gc.buttonRestart = buttonRestart
         gc.buttonNext = buttonNext
         gc.buttonPrevious = buttonPrevious
         gc.buttonMenu = buttonMenu
-
+        
+        gc.levelLabel = TextLabel("Level \(GameScene.level)", at: CGPoint(x: 384, y: 970))
+        gc.stepsLabel = TextLabel("Steps: \(gc.grid.steps)", at: CGPoint(x: 384, y: 150))
+        
+        setButtonHandlers()
+        addButtonsAndLabelsToSceneChildren()
+    }
+    
+    private func setButtonHandlers() {
+        gc.buttonRestart.selectedHandler = {
+            self.gc.gvc.loadLevel(number: GameScene.level)
+        }
+        
+        gc.buttonNext.selectedHandler = {
+            self.gc.goToNextLevel()
+        }
+        
+        gc.buttonPrevious.selectedHandler = {
+            self.gc.goToPreviousLevel()
+        }
+        
+        gc.buttonMenu.selectedHandler = {
+            self.gc.showPauseAndSettingsMenu()
+        }
+    }
+    
+    private func addButtonsAndLabelsToSceneChildren() {
         gc.addChild(gc.buttonRestart)
         gc.addChild(gc.buttonNext)
         gc.addChild(gc.buttonPrevious)
