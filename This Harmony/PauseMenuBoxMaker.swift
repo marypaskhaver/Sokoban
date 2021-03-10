7d259453f4bbdf46789ae1eb74c136e9cd7eaad5
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-03-10 12:08:18 -0500

    Add arg gameScene and create levelMenuLabel in func getBox

--- This Harmony/PauseMenuBoxMaker.swift
+++ This Harmony/PauseMenuBoxMaker.swift
@@ -10,11 +10,19 @@ import SpriteKit
 
 class PauseMenuBoxMaker {
     
-    func getBox() -> SKShapeNode {
+    func getBox(for gameScene: GameScene) -> SKShapeNode {
         let menuBox: SKShapeNode = SKShapeNode(rect: CGRect(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY, width: 500, height: 700))
         menuBox.zPosition = 2
         menuBox.fillColor = .white
         menuBox.name = "menu-box"
+        
+        let levelMenuLabel: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "level_menu_button"), CGSize(width: 200, height: 100), atPosition: CGPoint(x: menuBox.frame.midX, y: menuBox.frame.midY))
+
+        levelMenuLabel.selectedHandler = {
+            gameScene.gvc.presentLevelMenu()
+        }
+        
+        menuBox.addChild(levelMenuLabel)
         
         return menuBox
     }
