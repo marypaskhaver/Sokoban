ae742a5979b912320346d7929bfd4e7bf55e90d5
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-03-18 12:04:28 -0400

    Make checkmarkView UIImageView in Storyboard + LevelCell, load w/ resized checkmark img

--- This Harmony/LevelCell.swift
+++ This Harmony/LevelCell.swift
@@ -11,45 +11,41 @@ import Foundation
 class LevelCell: UICollectionViewCell {
     
     @IBOutlet weak var levelNumberLabel: UILabel!
-    @IBOutlet weak var checkmarkView: UIView!
+    @IBOutlet weak var checkmarkView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = .lightGray
+        
+        let checkmarkImage: UIImage = UIImage(named: "checkmark")!
+        let smallerCheckmarkImage: UIImage = resizeImage(image: checkmarkImage, targetSize: CGSize(width: 45, height: 45))
+        checkmarkView.image = smallerCheckmarkImage
     }
     
-    override func draw(_ rect: CGRect) {
-        drawRectChecked(rect: rect)
-    }
-    
-    func drawRectChecked(rect: CGRect) {
-        let context = UIGraphicsGetCurrentContext()
-        let checkmarkBlue2 = UIColor(red: 0.078, green: 0.435, blue: 0.875, alpha: 1)
-        let shadow2 = UIColor.black
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+       let size = image.size
 
-        let shadow2Offset = CGSize(width: 0.1, height: -0.1)
-        let shadow2BlurRadius = 2.5
-        let frame = self.bounds
-//        let group = CGRect(x: frame.minX + 3, y: frame.minY + 3, width: frame.width - 6, height: frame.height - 6)
-        let group = CGRect(x: frame.maxX - 19, y: frame.maxY - 20, width: frame.width / 3, height: frame.height / 3)
+       let widthRatio  = targetSize.width  / size.width
+       let heightRatio = targetSize.height / size.height
 
-        let checkedOvalPath = UIBezierPath(ovalIn: CGRect(x: group.minX + floor(group.width * 0.00000 + 0.5), y: group.minY + floor(group.height * 0.00000 + 0.5), width: floor(group.width * 1.00000 + 0.5) - floor(group.width * 0.00000 + 0.5), height: floor(group.height * 1.00000 + 0.5) - floor(group.height * 0.00000 + 0.5)))
+       // Figure out what our orientation is, and use that to form the rectangle
+       var newSize: CGSize
+       if(widthRatio > heightRatio) {
+           newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+       } else {
+           newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+       }
 
-        context!.saveGState()
-        context!.setShadow(offset: shadow2Offset, blur: CGFloat(shadow2BlurRadius), color: shadow2.cgColor)
-        checkmarkBlue2.setFill()
-        checkedOvalPath.fill()
-        context!.restoreGState()
-        UIColor.white.setStroke()
-        checkedOvalPath.lineWidth = 1
-        checkedOvalPath.stroke()
-        let bezierPath = UIBezierPath()
-        bezierPath.move(to: CGPoint(x: group.minX + 0.27083 * group.width, y: group.minY + 0.54167 * group.height))
-        bezierPath.addLine(to: CGPoint(x: group.minX + 0.41667 * group.width, y: group.minY + 0.68750 * group.height))
-        bezierPath.addLine(to: CGPoint(x: group.minX + 0.75000 * group.width, y: group.minY + 0.35417 * group.height))
-        bezierPath.lineCapStyle = CGLineCap.square
-        UIColor.white.setStroke()
-        bezierPath.lineWidth = 1.3
-        bezierPath.stroke()
-    }
+       // This is the rect that we've calculated out and this is what is actually used below
+       let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+       // Actually do the resizing to the rect using the ImageContext stuff
+       UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+       image.draw(in: rect)
+       let newImage = UIGraphicsGetImageFromCurrentImageContext()
+       UIGraphicsEndImageContext()
+
+       return newImage!
+   }
+
 }
