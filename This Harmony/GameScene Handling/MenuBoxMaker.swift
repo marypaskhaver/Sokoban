2dc1fa79d8a4bb107bbce629cad67e614a09ffde
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-09 18:13:40 -0400

    Change fontColor in levelCompleteMenu labels

--- This Harmony/GameScene Handling/MenuBoxMaker.swift
+++ This Harmony/GameScene Handling/MenuBoxMaker.swift
@@ -44,37 +44,37 @@ class MenuBoxMaker {
     private func getLevelCompleteMenu(for gameScene: GameScene) -> SKShapeNode {
         let menuBox: SKShapeNode = SKShapeNode(rect: CGRect(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY, width: 500, height: 500))
         menuBox.zPosition = 2
-        menuBox.fillColor = .black
+        menuBox.fillColor = .white
         
         let label: SKLabelNode = SKLabelNode(text: "New Steps: \(gameScene.grid.steps)")
         label.position = CGPoint(x: menuBox.frame.midX, y: menuBox.frame.midY)
-        label.fontSize = 50
+        label.fontSize = 60
         label.fontColor = SKColor(displayP3Red: 1.0, green: 0.3, blue: 0.3, alpha: 1.0)
         label.fontName = "PingFangSC-Semibold"
         
         for i in 1..<5 {
             let outline: SKLabelNode = SKLabelNode(text: label.text)
             outline.text = label.text
-            outline.fontSize = 50
-            outline.fontColor = .white
+            outline.fontSize = 60
+            outline.fontColor = SKColor(displayP3Red: 0.9, green: 0.8, blue: 0.125, alpha: 1.0)
             outline.fontName = "PingFangSC-Semibold"
 
-            let offAmt: CGFloat = 2
+            let offAmt: CGFloat = 2.5
             
-            if (i==1) {
-                outline.position = CGPoint(x: menuBox.frame.midX - offAmt, y: menuBox.frame.midY + offAmt);
+            if (i == 1) {
+                outline.position = CGPoint(x: menuBox.frame.midX - offAmt, y: menuBox.frame.midY + offAmt)
             }
             
-            if (i==2) {
-                outline.position = CGPoint(x: menuBox.frame.midX + offAmt, y: menuBox.frame.midY + offAmt);
+            if (i == 2) {
+                outline.position = CGPoint(x: menuBox.frame.midX + offAmt, y: menuBox.frame.midY + offAmt)
             }
                 
-            if (i==3) {
-                outline.position = CGPoint(x: menuBox.frame.midX - offAmt, y: menuBox.frame.midY - offAmt);
+            if (i == 3) {
+                outline.position = CGPoint(x: menuBox.frame.midX - offAmt, y: menuBox.frame.midY - offAmt)
             }
                 
-            if (i==4) {
-                outline.position = CGPoint(x: menuBox.frame.midX + offAmt, y: menuBox.frame.midY - offAmt);
+            if (i == 4) {
+                outline.position = CGPoint(x: menuBox.frame.midX + offAmt, y: menuBox.frame.midY - offAmt)
             }
             
             menuBox.addChild(outline)
