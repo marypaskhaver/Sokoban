35ee0bfcedc788f301bf98723025e0af6b0faece
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-09 14:48:41 -0400

    Create step labels w/ outlined text for levelCompleteMenu

--- This Harmony/GameScene Handling/MenuBoxMaker.swift
+++ This Harmony/GameScene Handling/MenuBoxMaker.swift
@@ -25,12 +25,12 @@ class MenuBoxMaker {
     }
     
     private func getPauseLevelMenu(for gameScene: GameScene) -> SKShapeNode {
-        let menuBox: SKShapeNode = SKShapeNode(rect: CGRect(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY, width: 500, height: 700))
+        let menuBox: SKShapeNode = SKShapeNode(rect: CGRect(x: UIScreen.main.bounds.midX - CGFloat(Constants.tileSize / 4), y: UIScreen.main.bounds.midY, width: 500, height: 500))
         menuBox.zPosition = 2
         menuBox.fillColor = .white
         menuBox.name = MenuBox.pauseLevelMenu.rawValue
         
-        let levelMenuLabel: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "level_menu_button"), CGSize(width: 200, height: 100), atPosition: CGPoint(x: menuBox.frame.midX, y: menuBox.frame.midY))
+        let levelMenuLabel: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "level_menu_button"), CGSize(width: menuBox.frame.width / 2, height: menuBox.frame.width / 6), atPosition: CGPoint(x: menuBox.frame.midX, y: menuBox.frame.midY))
 
         levelMenuLabel.selectedHandler = {
             gameScene.gvc.presentLevelMenu()
@@ -42,9 +42,45 @@ class MenuBoxMaker {
     }
     
     private func getLevelCompleteMenu(for gameScene: GameScene) -> SKShapeNode {
-        let menuBox: SKShapeNode = SKShapeNode(rect: CGRect(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY, width: 200, height: 500))
+        let menuBox: SKShapeNode = SKShapeNode(rect: CGRect(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY, width: 500, height: 500))
         menuBox.zPosition = 2
-        menuBox.fillColor = .red
+        menuBox.fillColor = .black
+        
+        let label: SKLabelNode = SKLabelNode(text: "New Steps: \(gameScene.grid.steps)")
+        label.position = CGPoint(x: menuBox.frame.midX, y: menuBox.frame.midY)
+        label.fontSize = 50
+        label.fontColor = SKColor(displayP3Red: 1.0, green: 0.3, blue: 0.3, alpha: 1.0)
+        label.fontName = "PingFangSC-Semibold"
+        
+        for i in 1..<5 {
+            let outline: SKLabelNode = SKLabelNode(text: label.text)
+            outline.text = label.text
+            outline.fontSize = 50
+            outline.fontColor = .white
+            outline.fontName = "PingFangSC-Semibold"
+
+            let offAmt: CGFloat = 2
+            
+            if (i==1) {
+                outline.position = CGPoint(x: menuBox.frame.midX - offAmt, y: menuBox.frame.midY + offAmt);
+            }
+            
+            if (i==2) {
+                outline.position = CGPoint(x: menuBox.frame.midX + offAmt, y: menuBox.frame.midY + offAmt);
+            }
+                
+            if (i==3) {
+                outline.position = CGPoint(x: menuBox.frame.midX - offAmt, y: menuBox.frame.midY - offAmt);
+            }
+                
+            if (i==4) {
+                outline.position = CGPoint(x: menuBox.frame.midX + offAmt, y: menuBox.frame.midY - offAmt);
+            }
+            
+            menuBox.addChild(outline)
+        }
+        
+        menuBox.addChild(label)
         
         return menuBox
     }
