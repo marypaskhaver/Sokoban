1ae02b893cb0522976d45c218dc0282a4ca1184d
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-09 22:10:01 -0400

    Create CoreDataManagerTests

---
+++ This HarmonyTests/CoreDataManagerTests.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataManagerTests.swift
+//  This HarmonyTests
+//
+//  Created by Mary Paskhaver on 4/9/21.
+//
+
+import XCTest
+import CoreData
+
+@testable import This_Harmony
+
+class CoreDataManagerTests: XCTestCase {
+    // MARK: - Class vars
+    var cdm: CoreDataManager!
+    
+    lazy var managedObjectModel: NSManagedObjectModel = MockDataModelObjects().managedObjectModel
+    lazy var mockPersistentContainer: NSPersistentContainer = MockDataModelObjects().persistentContainer
+
+    var saveNotificationCompleteHandler: ((Notification)->())?
+
+    // MARK: - Set up and tear down methods
+    override func setUp() {
+        super.setUp()
+        cdm = CoreDataManager(container: mockPersistentContainer)
+        
+        // Initialize data stubs here
+        
+        // When a context has an item saved to it, it puts out a notification
+        NotificationCenter.default.addObserver(self, selector: #selector(contextSaved(notification:)), name: NSNotification.Name.NSManagedObjectContextDidSave, object: nil )
+    }
+    
+    override func tearDown() {
+        // Flush data here
+        cdm = nil
+        super.tearDown()
+    }
+    
+    // MARK: - Context Notification methods
+    func contextSaved(notification: Notification ) {
+        saveNotificationCompleteHandler?(notification)
+    }
+        
+    func waitForSavedNotification(completeHandler: @escaping ((Notification)->()) ) {
+        saveNotificationCompleteHandler = completeHandler
+    }
+}
