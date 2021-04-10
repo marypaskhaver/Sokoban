b3892b270451d434c494ccf71dc4c25e54952e2e
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-09 21:39:52 -0400

    Add CoreDataManager class

---
+++ This Harmony/Data Model/CoreDataManager.swift
@@ -0,0 +1,45 @@
+//
+//  CoreDataManager.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 4/9/21.
+//
+
+import CoreData
+import UIKit
+
+class CoreDataManager {
+    let persistentContainer: NSPersistentContainer!
+    
+    // MARK: - Init with dependency
+    init(container: NSPersistentContainer) {
+        self.persistentContainer = container
+        self.persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    convenience init() {
+        //Use the default container for production environment
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError("Can not get shared app delegate")
+        }
+        
+        self.init(container: appDelegate.persistentContainer)
+    }
+    
+    // Commit changes in the background thread instead of main thread
+    lazy var backgroundContext: NSManagedObjectContext = {
+        return self.persistentContainer.newBackgroundContext()
+    }()
+    
+    // MARK: - Universal save
+    func save() {
+        if backgroundContext.hasChanges {
+            do {
+                try backgroundContext.save()
+            } catch {
+                print("Save error \(error)")
+            }
+        }
+
+    }
+}
