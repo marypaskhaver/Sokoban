0e331c9dfcc81d976861f00e8fc60ad95fadabdf
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-10 11:53:16 -0400

    Create CompletedLevel classes for CoreData entity

---
+++ This Harmony/Data Model/CompletedLevel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CompletedLevel+CoreDataClass.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 4/10/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CompletedLevel)
+public class CompletedLevel: NSManagedObject {
+
+}
---
+++ This Harmony/Data Model/CompletedLevel+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  CompletedLevel+CoreDataProperties.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 4/10/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CompletedLevel {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CompletedLevel> {
+        return NSFetchRequest<CompletedLevel>(entityName: "CompletedLevel")
+    }
+
+    @NSManaged public var lowestSteps: Int32
+
+}
