7b6d4e9297b2bdd4cc1ce630bab54c729bb00938
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-09 20:50:58 -0400

    Move StepLabel class from MenuBoxMaker to own file

--- This Harmony/GameScene Handling/MenuBoxMaker.swift
+++ This Harmony/GameScene Handling/MenuBoxMaker.swift
@@ -41,25 +41,6 @@ class MenuBoxMaker {
         return menuBox
     }
     
-    class StepLabel: SKLabelNode {
-                
-        init(withText text: String, at position: CGPoint) {
-            super.init()
-            
-            self.text = text
-            self.position = CGPoint(x: position.x, y: position.y)
-            self.fontSize = 60
-            self.horizontalAlignmentMode = .center
-            self.fontColor = SKColor(displayP3Red: 1.0, green: 0.3, blue: 0.3, alpha: 1.0)
-            self.fontName = "PingFangSC-Semibold"
-            self.zPosition = 3
-        }
-        
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-        }
-    }
-    
     private func getLevelCompleteMenu(for gameScene: GameScene) -> SKShapeNode {
         let menuBox: SKShapeNode = SKShapeNode(rect: CGRect(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY, width: 500, height: 500))
         menuBox.zPosition = 2
---
+++ This Harmony/Scene Buttons and Labels/StepLabel.swift
@@ -0,0 +1,27 @@
+//
+//  StepLabel.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 4/9/21.
+//
+
+import SpriteKit
+
+class StepLabel: SKLabelNode {
+            
+    init(withText text: String, at position: CGPoint) {
+        super.init()
+        
+        self.text = text
+        self.position = CGPoint(x: position.x, y: position.y)
+        self.fontSize = 60
+        self.horizontalAlignmentMode = .center
+        self.fontColor = SKColor(displayP3Red: 1.0, green: 0.3, blue: 0.3, alpha: 1.0)
+        self.fontName = "PingFangSC-Semibold"
+        self.zPosition = 3
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
