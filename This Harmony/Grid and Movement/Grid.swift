ed7c64b6c4050881cb432f949d01d4334657a2bc
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-11 16:59:10 -0400

    Use topLeftNode position to calc lpRowAndCol

--- This Harmony/Grid and Movement/Grid.swift
+++ This Harmony/Grid and Movement/Grid.swift
@@ -59,7 +59,9 @@ class Grid {
     func hideBlockedLaserBeams() {
         for lp in laserPointers {
             // This reloads all the laser pointers-- make it reload just the one(s) w/ a blocked beam
-            let lpRowAndCol: GridPoint = GridPoint(row: (-Int(lp.position.y) + 900) / Constants.tileSize, col: (Int(lp.position.x) - 139) / Constants.tileSize)
+            let topLeftNode: Tile = self.grid[0][0]
+            
+            let lpRowAndCol: GridPoint = GridPoint(row: (-Int(lp.position.y) + Int(topLeftNode.getRoundedY())) / Constants.tileSize, col: (Int(lp.position.x) - Int(topLeftNode.getRoundedX())) / Constants.tileSize)            
             let lsu: LaserSetterUpper = LaserSetterUpper(with: grid)
             let allFloorTilesInFrontOfLP: [Floor] = lsu.getAllFloorTilesInFrontOf(point: GridPoint(row: lpRowAndCol.row, col: lpRowAndCol.col), inDirection: lp.direction)
             let clearTiles: [Floor] = lsu.getClearFloorTiles(from: allFloorTilesInFrontOfLP)
