bdbc3f297dbf38991385bd981c25bc59d5694ff4
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-11 22:11:09 -0400

    Move moving crate code to CrateMover

--- This Harmony/Grid and Movement/PlayerMover.swift
+++ This Harmony/Grid and Movement/PlayerMover.swift
@@ -185,35 +185,7 @@ class PlayerMover {
     // square immediately below the current Floor.
     func moveCrateIfNeeded(onTile tile: Tile, inDirection dir: Direction) {
         if isFloorThatContainsCrate(tile) {
-            let floorThatHoldsCrateInFrontofPlayer: Floor = tile as! Floor
-            let positionOfFloorThatHoldsCrateInFrontOfPlayer: GridPoint = getRowAndColumnOfFloor(floorNodeInGrid: floorThatHoldsCrateInFrontofPlayer)
-            
-            switch dir {
-            case .up:
-                let oneTileUp: Floor = (grid[positionOfFloorThatHoldsCrateInFrontOfPlayer.row - 1][positionOfFloorThatHoldsCrateInFrontOfPlayer.col] as! Floor)
-                oneTileUp.setCrate(to: floorThatHoldsCrateInFrontofPlayer.crate!)
-                oneTileUp.crate?.isOnActiveLaserBeam = oneTileUp.laserBeams.filter( { !$0.isHidden } ).count > 0 ? true : false
-                (floorThatHoldsCrateInFrontofPlayer.crate!).moveUp(byNumTiles: 1)
-            case .down:
-                let oneTileDown: Floor = (grid[positionOfFloorThatHoldsCrateInFrontOfPlayer.row + 1][positionOfFloorThatHoldsCrateInFrontOfPlayer.col] as! Floor)
-                oneTileDown.setCrate(to: floorThatHoldsCrateInFrontofPlayer.crate!)
-                oneTileDown.crate?.isOnActiveLaserBeam = oneTileDown.laserBeams.filter( { !$0.isHidden } ).count > 0 ? true : false
-                (floorThatHoldsCrateInFrontofPlayer.crate!).moveDown(byNumTiles: 1)
-            case .left:
-                let oneTileLeft: Floor = (grid[positionOfFloorThatHoldsCrateInFrontOfPlayer.row][positionOfFloorThatHoldsCrateInFrontOfPlayer.col - 1] as! Floor)
-                oneTileLeft.setCrate(to: floorThatHoldsCrateInFrontofPlayer.crate!)
-                oneTileLeft.crate?.isOnActiveLaserBeam = oneTileLeft.laserBeams.filter( { !$0.isHidden } ).count > 0 ? true : false
-                (floorThatHoldsCrateInFrontofPlayer.crate!).moveLeft(byNumTiles: 1)
-            case .right:
-                let oneTileRight: Floor = (grid[positionOfFloorThatHoldsCrateInFrontOfPlayer.row][positionOfFloorThatHoldsCrateInFrontOfPlayer.col + 1] as! Floor)
-                oneTileRight.setCrate(to: floorThatHoldsCrateInFrontofPlayer.crate!)
-                oneTileRight.crate?.isOnActiveLaserBeam = oneTileRight.laserBeams.filter( { !$0.isHidden } ).count > 0 ? true : false
-                (floorThatHoldsCrateInFrontofPlayer.crate!).moveRight(byNumTiles: 1)
-            default:
-                print("Unknown direction")
-            }
-            
-            floorThatHoldsCrateInFrontofPlayer.setCrateToNil()
+            CrateMover(withGrid: grid).moveCrate(onTile: tile, inDirection: dir) // Test if grid, CompleteLevel, etc update fully
             didMoveCrate = true
         }
     }
