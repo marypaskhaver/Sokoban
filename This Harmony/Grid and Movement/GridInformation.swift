1ad9e98d2331ef3706ccbefdf8b5888b4ffea81b
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-13 22:05:21 -0400

    Create GridInformation w/ getPlayerRowAndCol, isFloorThatContainsCrate, and getRowAndColumnOfFloor funcs

---
+++ This Harmony/Grid and Movement/GridInformation.swift
@@ -0,0 +1,54 @@
+//
+//  GridInformation.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 4/13/21.
+//
+
+import Foundation
+
+class GridInformation {
+    
+    let grid: [[Tile]]
+    
+    init(withGrid grid: [[Tile]]) {
+        self.grid = grid
+    }
+    
+    func isFloorThatContainsCrate(_ tile: Tile) -> Bool {
+        if let floor = tile as? Floor {
+            return floor.crate != nil
+        }
+        
+        return false
+    }
+    
+    func getRowAndColumnOfFloor(floorNodeInGrid floorNode: Floor) -> GridPoint {
+        for row in 0..<grid.count {
+            for col in 0..<grid[row].count {
+                if grid[row][col] == floorNode {
+                    return GridPoint(row: row, col: col)
+                }
+            }
+        }
+        
+        return GridPoint(row: -1, col: -1)
+    }
+    
+    func getPlayerRowAndCol() -> GridPoint {
+        for row in 0..<grid.count {
+            for col in 0..<grid[row].count {
+                if grid[row][col] as? Floor != nil {
+                    let node: Floor = grid[row][col] as! Floor
+
+                    if node.player != nil {
+                        return GridPoint(row: row, col: col)
+                    }
+                }
+            }
+        }
+        
+        return GridPoint(row: -1, col: -1)
+    }
+    
+}
