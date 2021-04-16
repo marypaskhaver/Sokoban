693e7cb8dd85c2b341a2f41f86da5c3eb0fee5f6
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-16 09:35:19 -0400

    Create class PlayerMoverTests, move tests from This_HarmonyTests to it

---
+++ This HarmonyTests/PlayerMoverTests.swift
@@ -0,0 +1,98 @@
+//
+//  PlayerMoverTests.swift
+//  This HarmonyTests
+//
+//  Created by Mary Paskhaver on 4/16/21.
+//
+
+import XCTest
+import SpriteKit
+
+@testable import This_Harmony
+
+class PlayerMoverTests: XCTestCase {
+    var gc: GameViewController!
+
+    override func setUpWithError() throws {
+        super.setUp()
+        
+        gc = MockDataModelObjects().createGameViewController()
+    }
+    
+    func testPlayerCantMoveOutsideGridWallBounds() {
+        gc.loadLevel(number: 1)
+        let scene: GameScene = (gc.view as! SKView).scene as! GameScene
+        let mover: PlayerMover = PlayerMover(with2DArrayOfTiles: scene.grid.grid)
+        
+        let playerPosition: GridPoint = GridInformation(withGrid: mover.grid).getPlayerRowAndCol()
+        XCTAssertEqual(playerPosition.row, 1)
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .up))
+        
+        XCTAssertEqual(playerPosition.col, 1)
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .left))
+        
+        // Assumes levels are squares / rectangles, outer edges = Wall border
+        let maxRow: Int = scene.grid.grid.count - 2 // - 1 would have gotten us the wall; we're going to right next to the wall
+        let maxCol: Int = scene.grid.grid[maxRow].count - 2
+        
+        // Set player position to max value
+        let player: Player = ((scene.grid.grid[GridInformation(withGrid: mover.grid).getPlayerRowAndCol().row][GridInformation(withGrid: mover.grid).getPlayerRowAndCol().col]) as! Floor).player!
+        let topLeftNode: Tile = scene.grid.grid[0][0]
+        player.position = CGPoint(x: CGFloat(maxCol * Constants.tileSize) + topLeftNode.position.x, y: -(CGFloat(maxRow * Constants.tileSize) - topLeftNode.position.y))
+        
+        ((scene.grid.grid[GridInformation(withGrid: mover.grid).getPlayerRowAndCol().row][GridInformation(withGrid: mover.grid).getPlayerRowAndCol().col]) as! Floor).player = nil
+        ((scene.grid.grid[maxRow][maxCol]) as! Floor).player = player
+        
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .down))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .right))
+    }
+    
+    func testPlayerCantMoveThroughWall() {
+        gc.loadLevel(number: 3)
+        let scene: GameScene = (gc.view as! SKView).scene as! GameScene
+        let mover: PlayerMover = PlayerMover(with2DArrayOfTiles: scene.grid.grid)
+        
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .up))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .down))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .left))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .right))
+    }
+    
+    func testPlayerCantPushTwoCratesAtOnce() {
+        gc.loadLevel(number: 4)
+        let scene: GameScene = (gc.view as! SKView).scene as! GameScene
+        let mover: PlayerMover = PlayerMover(with2DArrayOfTiles: scene.grid.grid)
+        
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .up))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .down))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .left))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .right))
+    }
+    
+    func testPlayerCantPushCrateThroughWall() {
+        gc.loadLevel(number: 5)
+        let scene: GameScene = (gc.view as! SKView).scene as! GameScene
+        let mover: PlayerMover = PlayerMover(with2DArrayOfTiles: scene.grid.grid)
+        
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .up))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .down))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .left))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .right))
+    }
+    
+    func testPlayerCantMoveThroughLaserBeams() {
+        gc.loadLevel(number: 6)
+        let scene: GameScene = (gc.view as! SKView).scene as! GameScene
+        let mover: PlayerMover = PlayerMover(with2DArrayOfTiles: scene.grid.grid)
+        
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .up))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .down))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .left))
+        XCTAssertFalse(mover.canPlayerMove(inDirection: .right))
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        gc = nil
+    }
+}
