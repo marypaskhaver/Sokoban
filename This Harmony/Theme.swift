8f53adff700bbae12f74541bec25a2856aedd777
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-18 16:35:05 -0400

    Create Theme protocol

---
+++ This Harmony/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  This Harmony
+//
+//  Created by Mary Paskhaver on 4/18/21.
+//
+
+import Foundation
+
+protocol Theme {
+    var floorImage: String { get }
+//    var wallImage: String { get }
+//    var laserPointerImage: String { get }
+//    var laserBeamImage: String { get }
+}
