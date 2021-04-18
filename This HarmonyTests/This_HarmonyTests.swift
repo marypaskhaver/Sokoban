4a2fa58f4ccc898a490eed9b27affe4917f1e2ad
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-18 11:07:56 -0400

    Add testLevelCompleteMenuShowsWhenLevelComplete

--- This HarmonyTests/This_HarmonyTests.swift
+++ This HarmonyTests/This_HarmonyTests.swift
@@ -109,7 +109,23 @@ class This_HarmonyTests: XCTestCase {
         }
         
         XCTAssertTrue(scene.grid.isLevelComplete())
+    }
+    
+    func testLevelCompleteMenuShowsWhenLevelComplete() {
+        gc.loadLevel(number: 7)
+        let scene: GameScene = (gc.view as! SKView).scene as! GameScene
+        
+        var menuBoxes = scene.children.filter( { $0.name == MenuBox.levelCompleteMenu.rawValue } )
+        XCTAssertEqual(menuBoxes.count, 0)
 
+        // Have player push crate down onto only storage space
+        let swipeDownTracker: SwipeDownTracker = scene.trackers.filter( { type(of: $0) == SwipeDownTracker.self } )[0] as! SwipeDownTracker
+        swipeDownTracker.swipedDown(sender: UISwipeGestureRecognizer())
+
+        menuBoxes = scene.children.filter( { $0.name == MenuBox.levelCompleteMenu.rawValue } )
+
+        XCTAssertEqual(menuBoxes.count, 1)
+        XCTAssertTrue(scene.intersects(menuBoxes[0]))
     }
 
     override func tearDownWithError() throws {
