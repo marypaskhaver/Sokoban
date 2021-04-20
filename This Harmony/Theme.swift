b4440e8a1ccfac4448436c0631bed27c35131602
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-04-19 20:14:55 -0400

    Add Beach Theme

--- This Harmony/Theme.swift
+++ This Harmony/Theme.swift
@@ -25,3 +25,8 @@ class Default2: Theme {
     var floorImage: [String] = [Constants.TileNames.player.rawValue]
     var storageImage: [String] = [Constants.TileNames.wall.rawValue]
 }
+
+class Beach: Theme {
+    var floorImage: [String] = ["beach_floor1", "beach_floor2", "beach_floor3", "beach_floor4"]
+    var storageImage: [String] = [Constants.TileNames.storage.rawValue]
+}
