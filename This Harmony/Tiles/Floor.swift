e477631d1d92fdc3577ec455cc2b8291728e80f6
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-03 17:45:42 -0400

    Add static var defaultTexture, use in init as default parameter

--- This Harmony/Tiles/Floor.swift
+++ This Harmony/Tiles/Floor.swift
@@ -14,9 +14,9 @@ class Floor: Tile {
     var laserBeams: [LaserBeam] = [LaserBeam]()
     
     let textureImage: SKTexture
-
-    init(withTexture texture: SKTexture = SKTexture(imageNamed: Constants.TileNames.floor.rawValue)) {
-//    init(withTexture texture: SKTexture = SKTexture(imageNamed: Constants.levelThemes[GameScene.level]!.floorImage[0])) {
+    static var defaultTexture = SKTexture(imageNamed: Constants.levelThemes[GameScene.level]!.floorImage[0])
+    
+    init(withTexture texture: SKTexture = defaultTexture) {
         textureImage = texture
         super.init(texture: textureImage, name: Constants.TileNames.floor.rawValue)
     }
