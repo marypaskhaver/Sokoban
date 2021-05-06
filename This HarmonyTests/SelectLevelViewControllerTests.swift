b943f532e8f220d3e45592ad1dec0ea136f98d95
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-06 13:39:47 -0400

    Add testCompletedLevelsAreAddedToConstants, set SwipeTracker's vars to use MockDataModelObjects

--- This HarmonyTests/SelectLevelViewControllerTests.swift
+++ This HarmonyTests/SelectLevelViewControllerTests.swift
@@ -18,6 +18,7 @@ class SelectLevelViewControllerTests: XCTestCase {
         super.setUp()
         
         gvc = MockDataModelObjects().createGameViewController()
+        Floor.defaultTexture = SKTexture(imageNamed: Constants.TileNames.floor.rawValue)
     }
     
     func testSelectLevelViewControllerNumberOfCellsEqualsNumberOfLevels() {
@@ -26,7 +27,32 @@ class SelectLevelViewControllerTests: XCTestCase {
         slvc.collectionView.layoutIfNeeded()
       
         XCTAssertEqual(slvc.collectionView.visibleCells.count, MockDataModelObjects.MockConstants().numLevels)
+    }
+    
+    func testCompletedLevelsAreAddedToConstants() {
+        SwipeTracker.constants = MockDataModelObjects.MockConstants()
+        SwipeTracker.gameSceneClass = MockDataModelObjects.MockGameScene.self
 
+        gvc.loadLevel(number: 7)
+        
+        let scene: GameScene = (gvc.view as! SKView).scene as! GameScene
+        
+        // Have player push crate down onto only storage space
+        let swipeDownTracker: SwipeDownTracker = scene.trackers.filter( { type(of: $0) == SwipeDownTracker.self } )[0] as! SwipeDownTracker
+        swipeDownTracker.swipedDown(sender: UISwipeGestureRecognizer())
+
+        let slvc: SelectLevelViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "SelectLevelViewController")
+        slvc.constants = SwipeTracker.constants
+        slvc.collectionView.layoutIfNeeded()
+    
+        XCTAssert(slvc.constants.completeLevels.contains(7))
+        
+        let numCheckedCells = slvc.collectionView.visibleCells.filter { (cell: UICollectionViewCell) in
+            return !(cell as! LevelCell).checkmarkView.isHidden
+        }.count
+
+        XCTAssertEqual(slvc.collectionView.visibleCells.count, 12)
+        XCTAssertEqual(numCheckedCells, 1)
     }
 
 }
