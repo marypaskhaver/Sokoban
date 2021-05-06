a015197bc61b30f6177079301772c82be191bdb9
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-06 17:37:47 -0400

    In init, use CoreData to add to completeLevels

--- This Harmony/Constants.swift
+++ This Harmony/Constants.swift
@@ -30,12 +30,41 @@ class Constants {
     ]
     
     var numLevels: Int = 0
-
+    
     init() {
         let resourceURL = Bundle.main.resourceURL!
         let resourcesContent = (try? FileManager.default.contentsOfDirectory(at: resourceURL, includingPropertiesForKeys: nil)) ?? []
         let levelCount = resourcesContent.filter { $0.lastPathComponent.hasPrefix("Level_") }.count
         
         numLevels = levelCount
+        
+        // Load completeLevels
+        let allCompletedLevels: [CompletedLevel] = CoreDataManager().fetchAllCompletedLevelsInAscendingOrderAndLowestSteps()
+        print("ALL COMPLETED LEVELS")
+        for level in allCompletedLevels {
+            print("Level! Number: \(level.levelNumber), steps: \(level.lowestSteps)")
+        }
+        
+        print("----------------------------------------------------------------")
+        
+        print("UNIQUE LEVELS")
+        var uniqueCompleteLevels: [CompletedLevel] = []
+        var uniqueCompleteLevelNumbers: [Int] = []
+
+        for level in allCompletedLevels {
+            if !uniqueCompleteLevelNumbers.contains(Int(level.levelNumber)) {
+                uniqueCompleteLevels.append(level)
+                uniqueCompleteLevelNumbers.append(Int(level.levelNumber))
+                print("Unique level found! Number: \(level.levelNumber)")
+            }
+        }
+        
+        print("----------------------------------------------------------------")
+        
+        for level in uniqueCompleteLevels {
+            if level.lowestSteps < Int32.max {
+                completeLevels.append(Int(level.levelNumber))
+            }
+        }
     }
 }
