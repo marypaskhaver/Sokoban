f7e73cc6746aa6c6ae4c73144545f83d223964a8
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-06 17:39:42 -0400

    Use superclass's constants let in move functions

--- This Harmony/Tiles/MovableTile.swift
+++ This Harmony/Tiles/MovableTile.swift
@@ -9,21 +9,21 @@ import Foundation
 import SpriteKit
 
 class MovableTile: Tile {
-    
+        
     func moveRight(byNumTiles numTiles: Int) {
-        self.run(SKAction.moveBy(x: CGFloat(numTiles * Constants().tileSize), y: 0, duration: Constants().movementAnimationDuration))
+        self.run(SKAction.moveBy(x: CGFloat(numTiles * constants.tileSize), y: 0, duration: constants.movementAnimationDuration))
     }
     
     func moveLeft(byNumTiles numTiles: Int) {
-        self.run(SKAction.moveBy(x: CGFloat(-numTiles * Constants().tileSize), y: 0, duration: Constants().movementAnimationDuration))
+        self.run(SKAction.moveBy(x: CGFloat(-numTiles * constants.tileSize), y: 0, duration: constants.movementAnimationDuration))
     }
     
     func moveUp(byNumTiles numTiles: Int) {
-        self.run(SKAction.moveBy(x: 0, y: CGFloat(numTiles * Constants().tileSize), duration: Constants().movementAnimationDuration))
+        self.run(SKAction.moveBy(x: 0, y: CGFloat(numTiles * constants.tileSize), duration: constants.movementAnimationDuration))
     }
     
     func moveDown(byNumTiles numTiles: Int) {
-        self.run(SKAction.moveBy(x: 0, y: CGFloat(-numTiles * Constants().tileSize), duration: Constants().movementAnimationDuration))
+        self.run(SKAction.moveBy(x: 0, y: CGFloat(-numTiles * constants.tileSize), duration: constants.movementAnimationDuration))
     }
     
 }
