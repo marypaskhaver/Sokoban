4312b6c10e7534088378bf605960e16afb9834bc
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-11 18:10:05 -0400

    Override moveLeft func to add animation w/ walkl images

--- This Harmony/Tiles/Player.swift
+++ This Harmony/Tiles/Player.swift
@@ -9,6 +9,22 @@ import Foundation
 import SpriteKit
 
 class Player: MovableTile {
+    // R, L, U, D
+    
+    override func moveLeft(byNumTiles numTiles: Int) {
+        super.moveLeft(byNumTiles: numTiles)
+        
+        let currentTextureName: String = Constants().levelThemes[GameScene.level]!.playerImage
+        
+        let anim = SKAction.animate(with: [
+                    SKTexture(imageNamed: currentTextureName + "_walkl_1"),
+                    SKTexture(imageNamed: currentTextureName + "_walkl_2"),
+                    SKTexture(imageNamed: currentTextureName + "_walkl_stand")
+                    ], timePerFrame: 0.15)
+        
+        self.run(SKAction.repeat(anim, count: 1), withKey: "walkLeft")
+    }
+    
     override func moveDown(byNumTiles numTiles: Int) {
         super.moveDown(byNumTiles: numTiles)
         
@@ -17,7 +33,7 @@ class Player: MovableTile {
         let anim = SKAction.animate(with: [
                     SKTexture(imageNamed: currentTextureName + "_walkd_1"), // Assumes all images are named in similar format
                     SKTexture(imageNamed: currentTextureName + "_walkd_2"),
-                    self.texture! // Current texture when standing still
+                    SKTexture(imageNamed: currentTextureName) // Current texture when standing still-- right now, all player images load looking down
                     ], timePerFrame: 0.15)
         
         self.run(SKAction.repeat(anim, count: 1), withKey: "walkDown")
