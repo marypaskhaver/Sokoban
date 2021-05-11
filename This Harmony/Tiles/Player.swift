0eb73c6c86d8bdb0264058091062b12b8146a1c5
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-11 18:00:55 -0400

    Override moveDown func to add animation w/ walkd images

--- This Harmony/Tiles/Player.swift
+++ This Harmony/Tiles/Player.swift
@@ -9,5 +9,18 @@ import Foundation
 import SpriteKit
 
 class Player: MovableTile {
-
+    override func moveDown(byNumTiles numTiles: Int) {
+        super.moveDown(byNumTiles: numTiles)
+        
+        let currentTextureName: String = Constants().levelThemes[GameScene.level]!.playerImage
+        
+        let anim = SKAction.animate(with: [
+                    SKTexture(imageNamed: currentTextureName + "_walkd_1"), // Assumes all images are named in similar format
+                    SKTexture(imageNamed: currentTextureName + "_walkd_2"),
+                    self.texture! // Current texture when standing still
+                    ], timePerFrame: 0.15)
+        
+        self.run(SKAction.repeat(anim, count: 1), withKey: "walkDown")
+        
+    }
 }
