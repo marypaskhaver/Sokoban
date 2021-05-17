e7377ac98c96c67975ce3aea662de5770ef0fa6e
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-17 14:35:28 -0400

    Add camera node, set position to be middle of grid

--- This Harmony/GameScene Handling/GameScene.swift
+++ This Harmony/GameScene Handling/GameScene.swift
@@ -52,6 +52,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // Set up trackers to track UISwipeGestureRecognizers and move character, update grid when swipe occurs
         trackers = [SwipeRightTracker(for: self), SwipeLeftTracker(for: self), SwipeUpTracker(for: self), SwipeDownTracker(for: self)]
+        
+        // Add camera and make its position the center of the node grid
+        let cam = SKCameraNode()
+        
+        // Grid will always be a rectangle, so get x by averaging midX's of first and last node in row 0 and first and last node in column 0
+        let gridMidX: CGFloat = (grid.grid[0][0].frame.midX + grid.grid[0][grid.grid[0].count - 1].frame.midX) / 2
+        let gridMidY: CGFloat = (grid.grid[0][0].frame.minY + grid.grid[grid.grid.count - 1][0].frame.minY) / 2
+
+        cam.position = CGPoint(x: gridMidX, y: gridMidY)
+        self.camera = cam
+        self.addChild(cam)
     }
     
     // Load level
