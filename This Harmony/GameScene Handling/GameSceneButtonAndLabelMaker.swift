2cbe5239f9861e431bb2b51ee8abdd69ab847513
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-23 10:25:40 -0400

    Add vector, use level 1's grid to position prev, next, reset buttons in same spots despite camera zoom

--- This Harmony/GameScene Handling/GameSceneButtonAndLabelMaker.swift
+++ This Harmony/GameScene Handling/GameSceneButtonAndLabelMaker.swift
@@ -16,25 +16,37 @@ class GameSceneButtonAndLabelMaker {
     }
     
     func addButtonsAndLabels() {
+        
+        let gc1: GameScene = GameScene.getLevel(1)!
+        let gridCreator: GridCreator = GridCreator(withChildren: gc1.children)
+        let grid = Grid(with2DArrayOfTiles: gridCreator.grid, laserPointers: gridCreator.laserPointers, withCoreDataManager: CoreDataManager())
+
         // Camera is always set up before buttons and is the center of the grid
         let cameraPosition: CGPoint = gc.children.first(where: {$0.name == "camera"})!.position
-
+        
+        let vector: CGVector = CGVector(dx: UIScreen.main.bounds.maxX / 2 - cameraPosition.x, dy: UIScreen.main.bounds.maxY / 2 - cameraPosition.y)
+        
         // Zoom camera so each grid takes up same amt of space. Based off # columns-- how to address rows?
         let zoomInAction = SKAction.scale(to: CGFloat(gc.grid.grid[0].count) / 8.0, duration: 0)
         gc.children.first(where: {$0.name == "camera"})!.run(zoomInAction)
-        
+
         // Edit node size based on zoom action
         let sizeMultiplier: CGFloat = 1 + abs(1.0 - CGFloat(gc.grid.grid[0].count) / 8.0)
         
         let buttonSize = CGSize(width: 80 * sizeMultiplier, height: 80 * sizeMultiplier)
-        
-        let buttonPrevious: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "prev_button"), buttonSize, atPosition: CGPoint(x: gc.grid.grid[0][0].frame.midX * sizeMultiplier / 2, y: gc.grid.grid[gc.grid.grid.count - 1][0].frame.midY * sizeMultiplier / 2))
+            
+        let positionBeforeApplyingVector: CGFloat = grid.grid[grid.grid.count - 1][0].frame.midY * sizeMultiplier - 0.35 * sizeMultiplier * UIScreen.main.bounds.height * sizeMultiplier
+        let tileShift: CGFloat = abs(1 - sizeMultiplier) * CGFloat(Tile.constants.tileSize)
+    
+        let buttonPrevious: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "prev_button"), buttonSize, atPosition: CGPoint(x: grid.grid[0][0].frame.midX * sizeMultiplier / 2, y: positionBeforeApplyingVector - (75 * sizeMultiplier + vector.dy * sizeMultiplier) - tileShift))
+                
+        // 75 won't work for all hardcoding-- what about diff phone sizes, iPads?
         
         let buttonNext: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "next_button"), buttonSize, atPosition: CGPoint(x: buttonPrevious.position.x + 90 * sizeMultiplier, y: buttonPrevious.frame.midY))
 
         let buttonRestart: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "reset_button"), buttonSize, atPosition: CGPoint(x: buttonNext.position.x + 90 * sizeMultiplier, y: buttonPrevious.frame.midY))
 
-        let buttonMenu: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "menu_button"), CGSize(width: 60 * sizeMultiplier, height: 60 * sizeMultiplier), atPosition: CGPoint(x: buttonPrevious.frame.midX - 20, y: gc.grid.grid[0][0].frame.midY + 80 * sizeMultiplier))
+        let buttonMenu: MSButtonNode = MSButtonNode(SKTexture(imageNamed: "menu_button"), CGSize(width: 60 * sizeMultiplier, height: 60 * sizeMultiplier), atPosition: CGPoint(x: buttonPrevious.frame.midX - 20, y: grid.grid[0][0].frame.midY + 80 * sizeMultiplier - (75 + vector.dy)))
 
         buttonRestart.name = "reset_button"
         buttonNext.name = "next_button"
