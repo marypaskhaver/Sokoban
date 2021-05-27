11422f51a384e24153f9e57d00b8858240ddacd4
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-26 21:02:16 -0400

    Add crateImage var, set in Theme init, use in Beach init

--- This Harmony/Theme.swift
+++ This Harmony/Theme.swift
@@ -11,11 +11,13 @@ class Theme: Equatable {
     var floorImage: String // Same floor image goes underneath player and all crates
     var playerImage: String
     var laserBeamImage: String
+    var crateImage: String
     
-    init(floorImage: String = Constants.TileNames.floor.rawValue, playerImage: String = Constants.TileNames.player.rawValue, laserBeamImage: String = "laser_beam_pointing_up") {
+    init(floorImage: String = Constants.TileNames.floor.rawValue, playerImage: String = Constants.TileNames.player.rawValue, laserBeamImage: String = "laser_beam_pointing_up", crateImage: String = Constants.TileNames.crate.rawValue) {
         self.floorImage = floorImage
         self.playerImage = playerImage
         self.laserBeamImage = laserBeamImage
+        self.crateImage = crateImage
     }
     
     static func == (lhs: Theme, rhs: Theme) -> Bool {
@@ -45,7 +47,8 @@ class Beach: Theme {
     init(withFloorImage floorImage: String = "beach_floor1") {
         let playerImage: String = "beach_player"
         let laserBeamImage: String = "beach_laser_beam_pointing_up"
-
-        super.init(floorImage: floorImage, playerImage: playerImage, laserBeamImage: laserBeamImage)
+        let crateImage: String = "beach_crate"
+        
+        super.init(floorImage: floorImage, playerImage: playerImage, laserBeamImage: laserBeamImage, crateImage: crateImage)
     }
 }
