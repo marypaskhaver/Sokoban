9d3b8eb175ea1f2c777e60288c7504483f66f1a8
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-30 11:33:00 -0400

    Add let beachThemeOneStorage w/ new added file

--- This Harmony/Sounds/Sound.swift
+++ This Harmony/Sounds/Sound.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct Sound {
     static let playerCantMove = "player_cant_move.mp3"
+    static let beachThemeOneStorage = ["beach_1st_thA.mp3"]
     static let beachThemeTwoStorages = ["beach_2st_thA_pt1.mp3", "beach_2st_thA_pt2.mp3"]
     static let beachThemeThreeStorages = ["beach_3st_thA_pt1.mp3", "beach_3st_thA_pt2.mp3", "beach_3st_thA_pt3.mp3"]
 }
