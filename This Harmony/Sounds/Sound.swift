f4c4a986cefc6c0dd5231926de0ebfb8a0e1c850
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-30 15:56:30 -0400

    Rename beach 3 storages theme A

--- This Harmony/Sounds/Sound.swift
+++ This Harmony/Sounds/Sound.swift
@@ -15,5 +15,5 @@ struct Sound {
     static let beachThemeTwoStoragesA = ["beach_2st_thA_pt1.mp3", "beach_2st_thA_pt2.mp3"]
     static let beachThemeTwoStoragesB = ["beach_2st_thB_pt1.mp3", "beach_2st_thB_pt2.mp3"]
     
-    static let beachThemeThreeStoragesA = ["beach_3st_thA_pt1.mp3", "beach_3st_thA_pt2.mp3", "beach_3st_thA_pt3.mp3"]
+    static let darkThemeThreeStoragesA = ["dark_3st_thA_pt1.mp3", "dark_3st_thA_pt2.mp3", "dark_3st_thA_pt3.mp3"]
 }
