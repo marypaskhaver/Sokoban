5395fca4ee6eec9cc3bb3f3cf0a32afa24cab586
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-05-30 12:09:12 -0400

    Rename lets, add beachThemeTwoStoragesB w/ new mp3 files

--- This Harmony/Sounds/Sound.swift
+++ This Harmony/Sounds/Sound.swift
@@ -9,7 +9,11 @@ import Foundation
 
 struct Sound {
     static let playerCantMove = "player_cant_move.mp3"
+    
     static let beachThemeOneStorage = ["beach_1st_thA.mp3"]
-    static let beachThemeTwoStorages = ["beach_2st_thA_pt1.mp3", "beach_2st_thA_pt2.mp3"]
-    static let beachThemeThreeStorages = ["beach_3st_thA_pt1.mp3", "beach_3st_thA_pt2.mp3", "beach_3st_thA_pt3.mp3"]
+    
+    static let beachThemeTwoStoragesA = ["beach_2st_thA_pt1.mp3", "beach_2st_thA_pt2.mp3"]
+    static let beachThemeTwoStoragesB = ["beach_2st_thB_pt1.mp3", "beach_2st_thB_pt2.mp3"]
+    
+    static let beachThemeThreeStoragesA = ["beach_3st_thA_pt1.mp3", "beach_3st_thA_pt2.mp3", "beach_3st_thA_pt3.mp3"]
 }
