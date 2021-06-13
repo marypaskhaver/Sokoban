bc8cac242787edf899d5b69047f403cd3b3d0782
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-06-13 18:38:50 -0400

    Add static let darkThemeThreeStoragesB

--- This Harmony/Sounds/Sound.swift
+++ This Harmony/Sounds/Sound.swift
@@ -28,6 +28,7 @@ struct Sound {
     static let darkThemeTwoStoragesA = ["dark_2st_thA_pt1.mp3", "dark_2st_thA_pt2.mp3"]
 
     static let darkThemeThreeStoragesA = ["dark_3st_thA_pt1.mp3", "dark_3st_thA_pt2.mp3", "dark_3st_thA_pt3.mp3"]
+    static let darkThemeThreeStoragesB = ["dark_3st_thB_pt1.mp3", "dark_3st_thB_pt2.mp3", "dark_3st_thB_pt3.mp3"]
     
     // MARK: - Jungle Themes
     static let jungleThemeOneStorageA = ["jungle_1st_thA.mp3"]
