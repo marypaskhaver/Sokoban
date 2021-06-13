a8e0d5d735ffac07b1163eff07fb65abde49c998
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-06-13 11:49:32 -0400

    Add jungleThemeTwoStorageA and comments

--- This Harmony/Sounds/Sound.swift
+++ This Harmony/Sounds/Sound.swift
@@ -9,12 +9,18 @@ import Foundation
 
 struct Sound {
     static let playerCantMove = "player_cant_move.mp3"
-    
+
+    // MARK: - Beach Themes
     static let beachThemeOneStorageA = ["beach_1st_thA.mp3"]
-    static let jungleThemeOneStorageA = ["jungle_1st_thA.mp3"]
 
     static let beachThemeTwoStoragesA = ["beach_2st_thA_pt1.mp3", "beach_2st_thA_pt2.mp3"]
     static let beachThemeTwoStoragesB = ["beach_2st_thB_pt1.mp3", "beach_2st_thB_pt2.mp3"]
     
+    // MARK: - Dark Dimension Themes
     static let darkThemeThreeStoragesA = ["dark_3st_thA_pt1.mp3", "dark_3st_thA_pt2.mp3", "dark_3st_thA_pt3.mp3"]
+    
+    // MARK: - Jungle Themes
+    static let jungleThemeOneStorageA = ["jungle_1st_thA.mp3"]
+
+    static let jungleThemeTwoStoragesA = ["jungle_2st_thA_pt1.mp3", "jungle_2st_thA_pt2.mp3"]
 }
