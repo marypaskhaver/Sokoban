24153d498a03e3ad1c804576fdcf5cdb8c5ba534
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-06-13 11:33:58 -0400

    Rename beachThemeOneStorageB to jungleThemeOneStorageA

--- This Harmony/Sounds/Sound.swift
+++ This Harmony/Sounds/Sound.swift
@@ -11,7 +11,7 @@ struct Sound {
     static let playerCantMove = "player_cant_move.mp3"
     
     static let beachThemeOneStorageA = ["beach_1st_thA.mp3"]
-    static let beachThemeOneStorageB = ["beach_1st_thB.mp3"]
+    static let jungleThemeOneStorageA = ["jungle_1st_thA.mp3"]
 
     static let beachThemeTwoStoragesA = ["beach_2st_thA_pt1.mp3", "beach_2st_thA_pt2.mp3"]
     static let beachThemeTwoStoragesB = ["beach_2st_thB_pt1.mp3", "beach_2st_thB_pt2.mp3"]
