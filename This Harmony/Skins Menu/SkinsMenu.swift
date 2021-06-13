c93770df33c44d944ffa6285a4ea8a2ca7f16d80
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-06-12 22:15:29 -0400

    Add playerImageLeft and Right vars, change to image +1 and -1 from imageInd in images [String]

--- This Harmony/Skins Menu/SkinsMenu.swift
+++ This Harmony/Skins Menu/SkinsMenu.swift
@@ -25,11 +25,19 @@ class SkinsMenu: SKScene {
     var imageInd: Int = 0
 
     var nameLabel: SKLabelNode!
+
+    var playerImageLeft: SKSpriteNode!
+    var playerImageRight: SKSpriteNode!
     
     override func didMove(to view: SKView) {
 
         playerImage = self.childNode(withName: "playerImage") as! SKSpriteNode
+        playerImageLeft = self.childNode(withName: "playerImageLeft") as! SKSpriteNode
+        playerImageRight = self.childNode(withName: "playerImageRight") as! SKSpriteNode
+
+        // Don't set left image bc when view loads, start at imageInd 0
         self.playerImage.texture = SKTexture(imageNamed: self.images[imageInd] + "_d_stand")
+        self.playerImageRight.texture = SKTexture(imageNamed: self.images[imageInd + 1] + "_d_stand")
 
         // Set UI connections
         nameLabel = self.childNode(withName: "nameLabel") as! SKLabelNode
@@ -73,15 +81,30 @@ class SkinsMenu: SKScene {
     }
     
     func updateButtons() {
+        print("current index: \(imageInd)")
         if imageInd == 0 {
             buttonPrev.isHidden = true
+            playerImageLeft.isHidden = true
         } else if imageInd == images.count - 1 {
             buttonNext.isHidden = true
+            playerImageRight.isHidden = true
         } else {
             buttonNext.isHidden = false
             buttonPrev.isHidden = false
+            playerImageLeft.isHidden = false
+            playerImageRight.isHidden = false
         }
         
+        if imageInd - 1 >= 0 {
+            playerImageLeft.texture = SKTexture(imageNamed: self.images[imageInd - 1] + "_d_stand")
+        }
+        
+        if imageInd + 1 < images.count {
+            playerImageRight.texture = SKTexture(imageNamed: self.images[imageInd + 1] + "_d_stand")
+        }
+        
+        playerImageLeft.reloadInputViews()
+        playerImageRight.reloadInputViews()
         buttonNext.reloadInputViews()
         buttonPrev.reloadInputViews()
     }
