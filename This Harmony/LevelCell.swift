7d7d755066f064e671ae77daef5d29b476517b15
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-06-14 19:34:59 -0400

    Remove resizeImage func and call to it

--- This Harmony/LevelCell.swift
+++ This Harmony/LevelCell.swift
@@ -16,38 +16,8 @@ class LevelCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        let smallerCheckmarkImage: UIImage = resizeImage(image: checkmarkView.image!, targetSize: CGSize(width: self.frame.width / 2, height: self.frame.height / 2))
-        
-        checkmarkView.image = smallerCheckmarkImage
-                
+                        
         self.setNeedsLayout()
     }
     
-    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
-       let size = image.size
-
-       let widthRatio  = targetSize.width  / size.width
-       let heightRatio = targetSize.height / size.height
-
-       // Figure out what our orientation is, and use that to form the rectangle
-       var newSize: CGSize
-       if(widthRatio > heightRatio) {
-           newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-       } else {
-           newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
-       }
-
-       // This is the rect that we've calculated out and this is what is actually used below
-       let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-
-       // Actually do the resizing to the rect using the ImageContext stuff
-       UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-       image.draw(in: rect)
-       let newImage = UIGraphicsGetImageFromCurrentImageContext()
-       UIGraphicsEndImageContext()
-
-       return newImage!
-   }
-
 }
