d129932f5543145a04b9da79ef220dc6175e6d72
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-06-14 19:58:24 -0400

    Remove .mp3 from end of each element name in [String]s

--- This Harmony/Sounds/Sound.swift
+++ This Harmony/Sounds/Sound.swift
@@ -8,42 +8,42 @@
 import Foundation
 
 struct Sound {
-    static let playerCantMove = "player_cant_move.mp3"
+    static let playerCantMove = "player_cant_move"
 
     // MARK: - Beach Themes
-    static let beachThemeOneStorageA = ["beach_1st_thA.mp3"]
+    static let beachThemeOneStorageA = ["beach_1st_thA"]
 
-    static let beachThemeTwoStoragesA = ["beach_2st_thA_pt1.mp3", "beach_2st_thA_pt2.mp3"]
-    static let beachThemeTwoStoragesB = ["beach_2st_thB_pt1.mp3", "beach_2st_thB_pt2.mp3"]
-    static let beachThemeTwoStoragesC = ["beach_2st_thC_pt1.mp3", "beach_2st_thC_pt2.mp3"]
+    static let beachThemeTwoStoragesA = ["beach_2st_thA_pt1", "beach_2st_thA_pt2"]
+    static let beachThemeTwoStoragesB = ["beach_2st_thB_pt1", "beach_2st_thB_pt2"]
+    static let beachThemeTwoStoragesC = ["beach_2st_thC_pt1", "beach_2st_thC_pt2"]
 
-    static let beachThemeThreeStoragesA = ["beach_3st_thA_pt1.mp3", "beach_3st_thA_pt2.mp3", "beach_3st_thA_pt3.mp3"]
-    static let beachThemeThreeStoragesB = ["beach_3st_thB_pt1.mp3", "beach_3st_thB_pt2.mp3", "beach_3st_thB_pt3.mp3"]
-    static let beachThemeThreeStoragesC = ["beach_3st_thC_pt1.mp3", "beach_3st_thC_pt2.mp3", "beach_3st_thC_pt3.mp3"]
-    static let beachThemeThreeStoragesD = ["beach_3st_thD_pt1.mp3", "beach_3st_thD_pt2.mp3", "beach_3st_thD_pt3.mp3"]
+    static let beachThemeThreeStoragesA = ["beach_3st_thA_pt1", "beach_3st_thA_pt2", "beach_3st_thA_pt3"]
+    static let beachThemeThreeStoragesB = ["beach_3st_thB_pt1", "beach_3st_thB_pt2", "beach_3st_thB_pt3"]
+    static let beachThemeThreeStoragesC = ["beach_3st_thC_pt1", "beach_3st_thC_pt2", "beach_3st_thC_pt3"]
+    static let beachThemeThreeStoragesD = ["beach_3st_thD_pt1", "beach_3st_thD_pt2", "beach_3st_thD_pt3"]
 
-    static let beachThemeFourStoragesA = ["beach_4st_thA_pt1.mp3", "beach_4st_thA_pt2.mp3", "beach_4st_thA_pt3.mp3", "beach_4st_thA_pt4.mp3"]
+    static let beachThemeFourStoragesA = ["beach_4st_thA_pt1", "beach_4st_thA_pt2", "beach_4st_thA_pt3", "beach_4st_thA_pt4"]
 
     // MARK: - Dark Dimension Themes
-    static let darkThemeTwoStoragesA = ["dark_2st_thA_pt1.mp3", "dark_2st_thA_pt2.mp3"]
-    static let darkThemeTwoStoragesB = ["dark_2st_thB_pt1.mp3", "dark_2st_thB_pt2.mp3"]
+    static let darkThemeTwoStoragesA = ["dark_2st_thA_pt1", "dark_2st_thA_pt2"]
+    static let darkThemeTwoStoragesB = ["dark_2st_thB_pt1", "dark_2st_thB_pt2"]
 
-    static let darkThemeThreeStoragesA = ["dark_3st_thA_pt1.mp3", "dark_3st_thA_pt2.mp3", "dark_3st_thA_pt3.mp3"]
-    static let darkThemeThreeStoragesB = ["dark_3st_thB_pt1.mp3", "dark_3st_thB_pt2.mp3", "dark_3st_thB_pt3.mp3"]
-    static let darkThemeThreeStoragesC = ["dark_3st_thC_pt1.mp3", "dark_3st_thC_pt2.mp3", "dark_3st_thC_pt3.mp3"]
-    static let darkThemeThreeStoragesD = ["dark_3st_thD_pt1.mp3", "dark_3st_thD_pt2.mp3", "dark_3st_thD_pt3.mp3"]
-    static let darkThemeThreeStoragesE = ["dark_3st_thE_pt1.mp3", "dark_3st_thE_pt2.mp3", "dark_3st_thE_pt3.mp3"]
-    static let darkThemeThreeStoragesF = ["dark_3st_thF_pt1.mp3", "dark_3st_thF_pt2.mp3", "dark_3st_thF_pt3.mp3"]
+    static let darkThemeThreeStoragesA = ["dark_3st_thA_pt1", "dark_3st_thA_pt2", "dark_3st_thA_pt3"]
+    static let darkThemeThreeStoragesB = ["dark_3st_thB_pt1", "dark_3st_thB_pt2", "dark_3st_thB_pt3"]
+    static let darkThemeThreeStoragesC = ["dark_3st_thC_pt1", "dark_3st_thC_pt2", "dark_3st_thC_pt3"]
+    static let darkThemeThreeStoragesD = ["dark_3st_thD_pt1", "dark_3st_thD_pt2", "dark_3st_thD_pt3"]
+    static let darkThemeThreeStoragesE = ["dark_3st_thE_pt1", "dark_3st_thE_pt2", "dark_3st_thE_pt3"]
+    static let darkThemeThreeStoragesF = ["dark_3st_thF_pt1", "dark_3st_thF_pt2", "dark_3st_thF_pt3"]
 
     // MARK: - Jungle Themes
-    static let jungleThemeOneStorageA = ["jungle_1st_thA.mp3"]
+    static let jungleThemeOneStorageA = ["jungle_1st_thA"]
 
-    static let jungleThemeTwoStoragesA = ["jungle_2st_thA_pt1.mp3", "jungle_2st_thA_pt2.mp3"]
+    static let jungleThemeTwoStoragesA = ["jungle_2st_thA_pt1", "jungle_2st_thA_pt2"]
     
-    static let jungleThemeThreeStoragesA = ["jungle_3st_thA_pt1.mp3", "jungle_3st_thA_pt2.mp3", "jungle_3st_thA_pt3.mp3"]
-    static let jungleThemeThreeStoragesB = ["jungle_3st_thB_pt1.mp3", "jungle_3st_thB_pt2.mp3", "jungle_3st_thB_pt3.mp3"]
-    static let jungleThemeThreeStoragesC = ["jungle_3st_thC_pt1.mp3", "jungle_3st_thC_pt2.mp3", "jungle_3st_thC_pt3.mp3"]
-    static let jungleThemeThreeStoragesD = ["jungle_3st_thD_pt1.mp3", "jungle_3st_thD_pt2.mp3", "jungle_3st_thD_pt3.mp3"]
-    static let jungleThemeThreeStoragesE = ["jungle_3st_thE_pt1.mp3", "jungle_3st_thE_pt2.mp3", "jungle_3st_thE_pt3.mp3"]
-    static let jungleThemeThreeStoragesF = ["jungle_3st_thF_pt1.mp3", "jungle_3st_thF_pt2.mp3", "jungle_3st_thF_pt3.mp3"]
+    static let jungleThemeThreeStoragesA = ["jungle_3st_thA_pt1", "jungle_3st_thA_pt2", "jungle_3st_thA_pt3"]
+    static let jungleThemeThreeStoragesB = ["jungle_3st_thB_pt1", "jungle_3st_thB_pt2", "jungle_3st_thB_pt3"]
+    static let jungleThemeThreeStoragesC = ["jungle_3st_thC_pt1", "jungle_3st_thC_pt2", "jungle_3st_thC_pt3"]
+    static let jungleThemeThreeStoragesD = ["jungle_3st_thD_pt1", "jungle_3st_thD_pt2", "jungle_3st_thD_pt3"]
+    static let jungleThemeThreeStoragesE = ["jungle_3st_thE_pt1", "jungle_3st_thE_pt2", "jungle_3st_thE_pt3"]
+    static let jungleThemeThreeStoragesF = ["jungle_3st_thF_pt1", "jungle_3st_thF_pt2", "jungle_3st_thF_pt3"]
 }
